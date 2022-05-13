b8b246a7265d397944ac4a5c33ed77a3fdb85090
Aleksandr Pashkovskii <apashkovskii@infoblox.com>
2022-05-13 14:09:01 +0300

    fix endpoint names

--- src/api/main_api_router.py
+++ src/api/main_api_router.py
@@ -11,26 +11,26 @@ router = APIRouter(
 )
 
 
-@router.post("/make_request_by_list", response_model=list[dict])
+@router.post("/make-request-by_list", response_model=list[dict])
 async def get_responses_by_url_list(urls: list[str]):
     return await UrlHandler().make_request_by_urls(urls)
 
 
-@router.post("/save_url_response_to_db", response_model=list[typing.Any])
+@router.post("/save-url-response-to_db", response_model=list[typing.Any])
 async def save_url_responses_to_db(model_list: list[UrlDto]):
     return await UrlHandler().save_url_responses_to_db(model_list)
 
 
-@router.get("/get_all_url_responses", response_model=list[UrlEntity])
+@router.get("/get-all-url-responses", response_model=list[UrlEntity])
 async def get_all_url_responses():
     return await UrlHandler().get_all_url_responses()
 
 
-@router.delete("/delete_url_response_by_id/{url_id}", response_model=typing.Any)
+@router.delete("/delete-url-response-by-id/{url_id}", response_model=typing.Any)
 async def get_all_url_responses(url_id: int):
     return await UrlHandler().delete_url_response_by_id(url_id)
 
 
-@router.get("/fill_agg_table", response_model=typing.Any)
+@router.get("/fill-agg-table", response_model=typing.Any)
 async def get_all_url_responses():
     return await UrlHandler().fill_agg_table()
